ce15f7daf8a47f5a7c572953b7b93c641b8d0ab1
yhz108 <yhz@ubiqube.com>
2023-03-24 17:59:02 +0800

    Added Metadata and Controller

--- juju-webapp/src/main/java/com/ubiqube/juju/controller/JujuController.java
+++ juju-webapp/src/main/java/com/ubiqube/juju/controller/JujuController.java
@@ -29,15 +29,16 @@ public class JujuController {
 	private static final Logger LOG = LoggerFactory.getLogger(JujuController.class);
 
 	@PostMapping(value = "/cloud", consumes = { "multipart/form-data" }, produces = "application/json")
-	public ResponseEntity<String> addCloud(@RequestParam("file") @NotNull final MultipartFile file) throws IOException {
-		LOG.info("calling /add-cloud.");
+	public ResponseEntity<String> addCloud(@RequestParam("cloudname") @NotNull final String cloudname,
+			@RequestParam("file") @NotNull final MultipartFile file) throws IOException {
+		LOG.info("calling POST /cloud with cloudname={}",cloudname);
 		try (final WorkspaceService ws = new WorkspaceService();
 				InputStream is = file.getInputStream()) {
 			LOG.info("{}: Deploying payload.", ws.getId());
 			String filename = "openstack-cloud.yaml";
 			ws.pushPayload(is, filename);
 			LOG.info("{}: Call Install ", ws.getId());
-			final ProcessResult res = ws.addCloud(filename);
+			final ProcessResult res = ws.addCloud(cloudname, filename);
 			LOG.info("{}: add-cloud done.", ws.getId());
 			LOG.info(res.getStdout());
 			LOG.error(res.getErrout());
@@ -47,7 +48,7 @@ public class JujuController {
 
 	@GetMapping(value = "/cloud", produces = "application/json")
 	public ResponseEntity<String> clouds() {
-		LOG.info("calling /clouds");
+		LOG.info("calling GET /clouds");
 		try (final WorkspaceService ws = new WorkspaceService()) {
 			return ResponseEntity.ok(ws.clouds());
 		}
@@ -62,13 +63,14 @@ public class JujuController {
 	}
 
 	@PostMapping(value = "/credential", consumes = { "multipart/form-data" }, produces = "application/json")
-	public ResponseEntity<String> addCredential(@RequestParam("file") @NotNull final MultipartFile file) throws IOException {
+	public ResponseEntity<String> addCredential(@RequestParam("cloudname") @NotNull final String cloudname,
+			@RequestParam("file") @NotNull final MultipartFile file) throws IOException {
 		LOG.info("calling /add-credential");
 		try (final WorkspaceService ws = new WorkspaceService();
 				InputStream is = file.getInputStream()) {
 			String filename = "mycreds.yaml";
 			ws.pushPayload(is, filename);
-			final ProcessResult res = ws.addCredential(filename);
+			final ProcessResult res = ws.addCredential(cloudname, filename);
 			return ResponseEntity.ok(res.getStdout());
 		}
 	}
@@ -82,23 +84,61 @@ public class JujuController {
 	}
 
 	@PutMapping(value = "/credential", consumes = { "multipart/form-data" }, produces = "application/json")
-	public ResponseEntity<String> updateCredential(@RequestParam("file") @NotNull final MultipartFile file) throws IOException {
+	public ResponseEntity<String> updateCredential(@RequestParam("cloudname") @NotNull final String cloudname,
+			@RequestParam("file") @NotNull final MultipartFile file) throws IOException {
 		LOG.info("calling /update-credential");
 		try (final WorkspaceService ws = new WorkspaceService();
 				InputStream is = file.getInputStream()) {
 			String filename = "mycreds.yaml";
 			ws.pushPayload(is, filename);
-			final ProcessResult res = ws.updateCredential(filename);
+			final ProcessResult res = ws.updateCredential(cloudname, filename);
 			return ResponseEntity.ok(res.getStdout());
 		}
 	}
 
 	@DeleteMapping(value = "/credential/{name}", produces = "application/json")
-	public ResponseEntity<String> removeCredential(@PathVariable("name") final String name) {
+	public ResponseEntity<String> removeCredential(@RequestParam("cloudname") @NotNull final String cloudname, @PathVariable("name") final String name) {
 		LOG.info("calling /remove-credential/{}",name);
 		try (final WorkspaceService ws = new WorkspaceService()) {
-			return ResponseEntity.ok(ws.removeCredential(name));
+			return ResponseEntity.ok(ws.removeCredential(cloudname, name));
 		}
 	}
 
+	@PostMapping(value = "/metadata", produces = "application/json")
+	public ResponseEntity<String> genMetadata(@RequestParam("path") @NotNull final String path,
+			@RequestParam("imageId") @NotNull final String imageId, @RequestParam("osSeries") @NotNull final String osSeries,
+			@RequestParam("region") @NotNull final String region, @RequestParam("osAuthUrl") @NotNull final String osAuthUrl) {
+		LOG.info("calling /metadata");
+		try (final WorkspaceService ws = new WorkspaceService()) {
+			return ResponseEntity.ok(ws.genMetadata(path, imageId, osSeries, region, osAuthUrl));
+		}
+	}
+
+	@PostMapping(value = "/controller", produces = "application/json")
+	public ResponseEntity<String> addController(@RequestParam("imageId") @NotNull final String imageId,
+			@RequestParam("osSeries") @NotNull final String osSeries, @RequestParam("constraints") @NotNull final String constraints,
+			@RequestParam("cloudname") @NotNull final String cloudname, @RequestParam("controllername") @NotNull final String controllername,
+			@RequestParam("region") @NotNull final String region) {
+		LOG.info("calling /metadata");
+		try (final WorkspaceService ws = new WorkspaceService()) {
+			return ResponseEntity.ok(ws.addController(imageId, osSeries, constraints, cloudname, controllername, region));
+		}
+	}
+	
+	@GetMapping(value = "/controller", produces = "application/json")
+	public ResponseEntity<String> controllers() {
+		LOG.info("calling /controllers");
+		try (final WorkspaceService ws = new WorkspaceService()) {
+			return ResponseEntity.ok(ws.controllers());
+		}
+	}
+
+	@DeleteMapping(value = "/controller/{cloudname}/{name}", produces = "application/json")
+	public ResponseEntity<String> removeController(@PathVariable("cloudname") final String cloudname, @PathVariable("name") final String name) {
+		LOG.info("calling /remove-credential/{}",name);
+		try (final WorkspaceService ws = new WorkspaceService()) {
+			return ResponseEntity.ok(ws.removeController(cloudname, name));
+		}
+	}
+	
 }
